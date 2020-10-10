64e3db41d2883c2378f19c263ad0bbeacfae5297
BuildTools <unconfigured@null.spigotmc.org>
2020-10-10 16:29:04 +0200

    VERSION ALPHA 1.0
    Movement implemented

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const entityName = document.querySelector("#newEntity");
+const entityInit = document.querySelector("#init");
+const clearInitBtn = document.querySelector("#init-clear");
 
 const newEnemyBtn = document.querySelector("#enemy");
 const newAllyBtn = document.getElementById("ally");
@@ -6,68 +9,193 @@ const deleteBtn = document.getElementById("delete");
 const saveMapBtn = document.getElementById("save");
 const clearBtn = document.getElementById("clear");
 
-let fields = document.querySelectorAll(".element");
-let row = 0;
-let col = 0;
+
 let tool = "";
 
+var row = 0;
+var col = 0;
 
-// Field Actions
-function Object(row,col){
-    this.position =[row,col];
-}
+  //CREATE VIRTUAL ARRAY
+var grid = new Array(10);
 
-function Enemy(row,col){
-    this.position =[row,col];
-}
+for (var i = 0; i < grid.length; i++) { 
+    grid[i] = new Array(10); 
+} 
+  
+for (var i = 0; i < 10; i++) { 
+    for (var j = 0; j < 10; j++) { 
+        grid[i][j] = new Field(i,j,undefined);
+    } 
+} 
 
-function Ally(row,col){
-    this.position =[row,col];
-}
 
-//  Field Interaction Handler
+//FUNCTIONS
 
-fields.forEach( field => {
-    field.addEventListener("click",function(ev){
-        row = (ev.currentTarget.className.slice(-1));
-        col = (ev.target.parentNode.id.slice(-1));
-        let field = document.querySelector(`.field${row}.col${col}`);
+//update grid from array
+function gridLoad(){
+    grid.forEach( line => {
+        line.forEach(tile =>{
+            if (tile.content instanceof UIObject){
+                tile.field.style.background = "black";
+                tile.field.textContent = "";
+                tile.buttons = undefined;
 
-        console.log(`row:${row} col:${col}`);
+            }else if(tile.content instanceof Ally){
+                tile.field.style.color = "green";
+                tile.field.textContent = `${tile.content.name.slice(0,1)}${tile.content.name.slice(-1)}`;
+                tile.buttons = new moveButtons();
+                tile.field.appendChild(tile.buttons.up);
+                tile.field.appendChild(tile.buttons.right);
+                tile.field.appendChild(tile.buttons.down);
+                tile.field.appendChild(tile.buttons.left);
 
-        //Event BRANCHING
-        switch(tool) {
+            }else if(tile.content instanceof Enemy){
+                tile.field.style.color = "red";
+                tile.field.textContent = `${tile.content.name.slice(0,1)}${tile.content.name.slice(-1)}`;
+                tile.buttons = new moveButtons();
+                tile.field.appendChild(tile.buttons.up);
+                tile.field.appendChild(tile.buttons.right);
+                tile.field.appendChild(tile.buttons.down);
+                tile.field.appendChild(tile.buttons.left);
 
-            case "delete":
-                field.textContent = "";
-                field.style.color = "black";
-                field.style.background = "white";
-            break;
-
-            case "enemy":
-                field.textContent = "E";
-                field.style.color = "red";
-                field.style.background = "white";
-            break;
-
-            case "ally":
-                field.textContent = "A";
-                field.style.color = "green";
-                field.style.background = "white";
-            break;
-
-            case "object":
-                field.textContent = "";
-                field.style.background = "black";
-            break;
-
-            default:
-
-            break;
-        }
+            }else{
+                    tile.field.style.color = "black";
+                    tile.field.style.background = "white";
+                    tile.field.textContent = "";
+            }
+        });
     });
-});
+    
 
+    //BUTTON MOVE HANDLING
+    const moveBtns = document.querySelectorAll(".moveButton");
+
+    if (moveBtns.length > 0){
+        moveBtns.forEach(movebtn =>{
+            movebtn.addEventListener("click",function(e){
+
+                row = parseInt(e.target.parentNode.className.slice(-1));
+                col = parseInt(e.target.parentNode.className.slice(11,12));
+                let currentField = grid[row][col].content;
+                
+                //UP
+                if(e.target.classList.contains("up")){
+                    if(row==0){
+                        alert("Can't move there");
+                        return;
+                    }
+
+                    let targetField = grid[row-1][col].content;
+
+                    if (targetField === undefined){
+                        let buffer = targetField;
+                        grid[row-1][col].content = currentField;
+                        grid[row][col].content = targetField;
+                        
+                    }else{
+                        alert("Can't move there");
+                    }
+                }
+
+                //RIGHT
+                if(e.target.classList.contains("right")){
+                     if(col==9){
+                        alert("Can't move there");
+                        return;
+                    }
+
+                    let targetField = grid[row][col+1].content;
+                    
+                    if (targetField === undefined){
+                        let buffer = targetField;
+                        grid[row][col+1].content = currentField;
+                        grid[row][col].content = targetField;
+                        
+                    }else{
+                        alert("Can't move there");
+                    }
+                    
+                }
+
+                //DOWN
+                if(e.target.classList.contains("down")){
+                    if(row==9){
+                        alert("Can't move there");
+                        return;
+                    }
+
+                    let targetField = grid[row+1][col].content;
+                    
+                    if (targetField === undefined){
+                        let buffer = targetField;
+                        grid[row+1][col].content = currentField;
+                        grid[row][col].content = targetField;
+                        
+                    }else{
+                        alert("Can't move there");
+                    }
+                    
+                }
+
+                //LEFT
+                if(e.target.classList.contains("left")){
+                    if(col==0){
+                        alert("Can't move there");
+                        return;
+                    }
+
+                    let targetField = grid[row][col-1].content;
+                    
+                    if (targetField === undefined){
+                        let buffer = targetField;
+                        grid[row][col-1].content = currentField;
+                        grid[row][col].content = targetField;
+                        
+                    }else{
+                        alert("Can't move there");
+                    }
+                    
+                }
+
+            })
+        })
+    }
+}
+
+//delete content by grid coordinate
+function contentDelete(line,tile){
+    if (grid[line][tile].content != undefined){
+        if (grid[line][tile].content.item != undefined){
+        grid[line][tile].content.item.remove();
+        }
+    grid[line][tile].content = undefined;
+    }
+
+    gridLoad();
+}
+
+//check for unique EntityName
+function checkUnique(name){
+    let unique = true;
+    grid.forEach( line => {
+        line.forEach( tile =>{
+            row = grid.indexOf(line);
+            col = line.indexOf(tile);
+            if (grid[row][col].content != undefined){
+                if (grid[row][col].content.name != undefined){
+                    if ((grid[row][col].content.name) === name){
+                        unique = false;
+                        
+                        alert("Entity Names must be unique")
+                        
+                    }
+                }
+            }
+        });
+    });
+
+    return unique;
+}
 
 //   TOOL HIGHLIGHTING
 function buttonSelect(){
@@ -83,39 +211,165 @@ function buttonSelect(){
     });
 }
 
-//// CLICK LISTENERS
-//  DELETE OBJECT
-deleteBtn.addEventListener("click",function(e){
-    tool = "delete";
-    buttonSelect();
-});
 
-// CREATE ENEMY
-newEnemyBtn.addEventListener("click",function(e){
-    tool = "enemy";
-    buttonSelect();
-});
+// CONSTRUCTORS
+function Field(row,col,content){
+    this.field = document.querySelector(`.field${i}.col${j}`);
+    this.content = content;
+    this.buttons = undefined;
+}
 
-// CREATE ALLY
-newAllyBtn.addEventListener("click",function(e){
-    //console.log(e.target.classList);
-    tool = "ally";
-    buttonSelect();
-});
+function UIObject(){
+    this.name = "wall";
+}
 
-// CREATE OBJECT
-newObjectBtn.addEventListener("click",function(){
-    tool = "object";
-    buttonSelect();
-});
+function Enemy(name,init){
 
-// CLEAR FIELD
-clearBtn.addEventListener("click",function(){
-    tool = "";
-    buttonSelect();
-    fields.forEach(field =>{
-        field.textContent = "";
-        field.style.color = "black";
-        field.style.background = "white";
+    this.name = name;
+    this.init = init;
+    
+    this.item = document.createElement("li");
+    this.item.className = "list-item";
+    this.item.appendChild(document.createTextNode(`${name} \n init: ${init}`));
+    document.querySelector(".collection").appendChild(this.item);
+}
+
+function Ally(name,init){
+
+        this.name = name;
+        this.init = init;
+        
+        this.item = document.createElement("li");
+        this.item.className = "list-item";
+        this.item.appendChild(document.createTextNode(`${name}\n init: ${init}`));
+        document.querySelector(".collection").appendChild(this.item);
+    
+}
+
+function moveButtons(){
+    this.up = document.createElement("div");
+    this.up.className = "moveButton up";
+
+    this.right = document.createElement("div");
+    this.right.className = "moveButton right";
+
+    this.down = document.createElement("div");
+    this.down.className = "moveButton down";
+
+    this.left = document.createElement("div");
+    this.left.className = "moveButton left";
+}
+
+
+
+// INTERACTION HANDLER
+grid.forEach( line => {
+    line.forEach( tile =>{
+        tile.field.addEventListener("click",function(ev){
+            gridLoad();
+            row = grid.indexOf(line);
+            col = line.indexOf(tile);
+            
+            switch(tool) {
+
+                case "delete":
+                    contentDelete(row,col);
+                    gridLoad();
+                break;
+    
+                case "enemy":
+                     if(entityInit.value === undefined || entityName.value === ""){
+                        alert("Enter Entity Data");
+                        break;
+                    }
+                    if(parseInt(entityInit.value) < 1){
+                        alert("Initiative needs to be bigger than 0")
+                        break;
+                    }
+
+                    if (checkUnique(entityName.value)){
+                        contentDelete(grid.indexOf(line),line.indexOf(tile));
+                        tile.content = new Enemy(entityName.value,entityInit.value);
+                    }
+                    gridLoad();
+                break;
+    
+                case "ally":
+                    if(entityInit.value === undefined || entityName.value === ""){
+                        alert("Enter Entity Data");
+                        break;
+                    }
+                    if(parseInt(entityInit.value) < 1){
+                        alert("Initiative needs to be bigger than 0")
+                        break;
+                    }
+                    if (checkUnique(entityName.value)){
+                        contentDelete(grid.indexOf(line),line.indexOf(tile));
+                        tile.content = new Ally(entityName.value,entityInit.value);
+                    }
+                    gridLoad();
+                break;
+    
+                case "object":
+                    contentDelete(grid.indexOf(line),line.indexOf(tile));
+                    tile.content = new UIObject();
+                    gridLoad();
+                break;
+    
+                default:
+    
+                break;
+            }
+        })
     })
-});
+})
+
+// EVENT LISTENERS
+function loadEventListeners(){
+
+    //TOOL DESELECTING BY CLICKING OUT
+    document.querySelector("body").addEventListener("click",function(e){
+        if ((e.target.classList.contains("element") === false)&&(e.target.classList.contains("button") === false)){
+            tool = "";
+            buttonSelect();
+        }
+    })
+    
+    // CREATE ENEMY
+    deleteBtn.addEventListener("click",function(e){
+        tool = "delete";
+        buttonSelect();
+    });
+
+    // CREATE ENEMY
+    newEnemyBtn.addEventListener("click",function(e){
+        tool = "enemy";
+        buttonSelect();
+    });
+
+    // CREATE ALLY
+    newAllyBtn.addEventListener("click",function(e){
+        //console.log(e.target.classList);
+        tool = "ally";
+        buttonSelect();
+    });
+
+    // CREATE OBJECT
+    newObjectBtn.addEventListener("click",function(){
+        tool = "object";
+        buttonSelect();
+    });
+
+    // CLEAR FIELD
+    clearBtn.addEventListener("click",function(){
+        tool = "";
+        buttonSelect();
+        grid.forEach( line => {
+            line.forEach( tile =>{
+                contentDelete(grid.indexOf(line),line.indexOf(tile));
+        })})
+        gridLoad();
+    });
+}
+
+loadEventListeners();
